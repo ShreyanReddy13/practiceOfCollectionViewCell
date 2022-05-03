9a071ba7b27366ddf46ba2a910749a7a53b60ab1
prathima <prathima.reddy.jc@gmail.com>
2022-05-03 10:54:01 +0530

    Initial Commit

---
+++ practiceOfCollectionViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practiceOfCollectionViewCell
+//
+//  Created by Prathima Juturu Chinna on 03/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
